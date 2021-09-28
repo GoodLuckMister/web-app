0d08b593eee174465f03e7002d5ec4d1e60fc1ae
Alexey <paet14@gmail.com>
2021-09-29 01:17:41 +0300

    add header

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useMediaQuery } from 'react-responsive';
 import { useHistory } from 'react-router-dom';
 
 import { FlexRowSpaceBetween } from '../components/FlexContainers';
@@ -14,6 +15,15 @@ interface IMenu {
     tabs: { label: string; link: string }[];
 }
 
+const Desktop = ({ children }) => {
+    const isTablet = useMediaQuery({ minWidth: 481 });
+    return isTablet ? children : null;
+};
+const Mobile = ({ children }) => {
+    const isMobile = useMediaQuery({ maxWidth: 480 });
+    return isMobile ? children : null;
+};
+
 const Menu: React.FC<IMenu> = ({ tabs, style }) => {
     const history = useHistory();
     const uiStore = useUIStore();
@@ -27,21 +37,46 @@ const Menu: React.FC<IMenu> = ({ tabs, style }) => {
         }
     };
     return (
-        <div style={style}>
-            <FlexRowSpaceBetween>
-                {tabs.map((tab, index) => (
-                    <Tab
-                        key={index}
-                        active={active?.label === tab.label}
-                        text={tab.label}
-                        link={tab.link}
-                    />
-                ))}
-                <PrimaryButton style={{ width: 156, marginLeft: 15 }} onClick={onStartClick}>
-                    Apply as TopiPro
-                </PrimaryButton>
-            </FlexRowSpaceBetween>
-        </div>
+        <>
+            <Mobile>
+                <div style={style}>
+                    <FlexRowSpaceBetween>
+                        {tabs.map((tab, index) => (
+                            <Tab
+                                key={index}
+                                active={active?.label === tab.label}
+                                text={tab.label}
+                                link={tab.link}
+                            />
+                        ))}
+                        <PrimaryButton
+                            style={{ width: 100, marginLeft: 10 }}
+                            onClick={onStartClick}>
+                            Apply as TopiPro
+                        </PrimaryButton>
+                    </FlexRowSpaceBetween>
+                </div>
+            </Mobile>
+            <Desktop>
+                <div style={style}>
+                    <FlexRowSpaceBetween>
+                        {tabs.map((tab, index) => (
+                            <Tab
+                                key={index}
+                                active={active?.label === tab.label}
+                                text={tab.label}
+                                link={tab.link}
+                            />
+                        ))}
+                        <PrimaryButton
+                            style={{ width: 156, marginLeft: 15 }}
+                            onClick={onStartClick}>
+                            Apply as TopiPro
+                        </PrimaryButton>
+                    </FlexRowSpaceBetween>
+                </div>
+            </Desktop>
+        </>
     );
 };
 export default Menu;
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,10 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+import './navbar.scss';
+
 import styled from '@emotion/styled';
 import React from 'react';
+import { useMediaQuery } from 'react-responsive';
 import { useHistory } from 'react-router-dom';
 
 import logo from '../assets/img/logo.png';
@@ -12,7 +15,6 @@ import { FlexRow } from './FlexContainers';
 import Menu from './Menu';
 
 const Wrapper = styled.header`
-    width: 100%;
     min-height: 100px;
     box-sizing: border-box;
     display: flex;
@@ -21,29 +23,77 @@ const Wrapper = styled.header`
     background-color: ${colors.almostBlack};
     transition: all 0.7s ease-in;
 `;
+const Mobiled = styled.header`
+    width: 480px;
+    min-height: 60px;
+    box-sizing: border-box;
+    padding: 15px;
+    background-color: ${colors.almostBlack};
+`;
 const tabs = Object.values(Routes)
     .filter((route) => route.menu)
     .map((route) => ({ label: route.label, link: route.link }));
+
+const Desktop = ({ children }) => {
+    const isTablet = useMediaQuery({ minWidth: 481 });
+    return isTablet ? children : null;
+};
+const Mobile = ({ children }) => {
+    const isMobile = useMediaQuery({ maxWidth: 480 });
+    return isMobile ? children : null;
+};
 const Navbar = () => {
     const history = useHistory();
     const [width] = useWindowSize();
 
     return (
-        <Wrapper>
-            <FlexRow style={{ margin: `12px ${width / 12}px 0px ${width / 9}px`, width: '100%' }}>
-                <img
-                    src={logo}
-                    alt="logo"
-                    onClick={() =>
-                        history.push({
-                            pathname: MainRoute.link
-                        })
-                    }
-                    style={{ cursor: 'pointer' }}
-                />
-                <Menu style={{ marginLeft: 'auto' }} tabs={tabs} />
-            </FlexRow>
-        </Wrapper>
+        <>
+            <Mobile>
+                <Mobiled>
+                    <div style={{ textAlign: 'center' }}>
+                        <img
+                            src={logo}
+                            alt="logo"
+                            onClick={() =>
+                                history.push({
+                                    pathname: MainRoute.link
+                                })
+                            }
+                            width="100px"
+                            style={{ cursor: 'pointer' }}
+                        />
+                    </div>
+                    <Menu
+                        tabs={tabs}
+                        style={{
+                            fontSize: '14px'
+                        }}
+                    />
+                </Mobiled>
+            </Mobile>
+            <Desktop>
+                <Wrapper>
+                    <FlexRow
+                        className="window-nav"
+                        style={{
+                            margin: `12px ${width / 12}px 0px ${width / 9}px`,
+                            width: '100%'
+                        }}>
+                        <img
+                            src={logo}
+                            alt="logo"
+                            onClick={() =>
+                                history.push({
+                                    pathname: MainRoute.link
+                                })
+                            }
+                            style={{ cursor: 'pointer' }}
+                        />
+                        <Menu style={{ marginLeft: 'auto' }} tabs={tabs} />
+                    </FlexRow>
+                </Wrapper>
+            </Desktop>
+        </>
     );
 };
 export default Navbar;
